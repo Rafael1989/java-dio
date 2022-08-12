1f0d173dcf7287f4d9b2e8337caa9702cd8a94fa
Rafael <flns_rafa@hotmail.com>
2022-08-12 18:01:55 +0200

    java

---
+++ vto/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,48 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        //int i;
+        int I;
+        //int 1n;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn12313md;
+        //int asdasd 78as;
+        int asdasd123123_md;
+        //int asasdas$%_md;
+
+        asrn12313md = 100;
+        asdasd123123_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn12313md);
+        System.out.println(asdasd123123_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
